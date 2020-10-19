7a93fa8e05d097f60158cf4cfc73cba1e94ae3a8
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-19 19:39:24 -0300

    More on logging class

--- SparkTraining/HelloSpark.py
+++ SparkTraining/HelloSpark.py
@@ -2,5 +2,16 @@ import sys
 from pyspark.sql import *
 
 if __name__ == "__main__":
+
+    # O método builder retorna um Builder Object que permite que você configure a sua Spark Session
+    # Spark Session é um SINGLETON OBJECT - só pode ter uma Spark Session ativa por aplicação Spark
+    # ALguams configurações como appName e master são as mais utilizadas por quase toda aplicação
+
+
+    spark = SparkSession.builder \
+        .appName("Spark Training") \
+        .master("local[3]") \
+        .getOrCreate()
+
     print("Starting Hello Spark.")
 
